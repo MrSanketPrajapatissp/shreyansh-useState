ebc04c0e1edc7bccd91aba693120dc587204a51e
MrSanketPrajapatissp <125129649+mrsanketprajapati@users.noreply.github.com>
2025-08-02 15:49:02 +0530

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,24 @@
 import Card from "./components/Card";
 import "./App.css";
-
+import { useState } from "react";
 
 function App() {
-
-
+  const [banned, setBanned] = useState({ name: "John", isBanned: false });
 
   return (
-    <>
-      <Card/>
-    </>
+    <div className="px-4 py-2 ">
+      <div className="px-4 py-2"> {banned.name}</div>
+      <div className="px-4 py-2 ">{banned.isBanned.toString()}</div>
+
+      <button
+        onClick={() => setBanned({ ...banned, isBanned: !banned.isBanned })}
+        className={`${
+          banned.isBanned ? "bg-blue-500" : "bg-red-500"
+        } text-white px-2 py-1 rounded-full`}
+      >
+        Change
+      </button>
+    </div>
   );
 }
 
--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,29 +1,9 @@
 import React from 'react'
 
 function Card() {
-
-  const onhandler = () => {
-    alert("Download started!");
-  }
-
-  const data = [
-    {name: "Mahi ve", description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit."},
-    {name: "O rabba ve", description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit."}
-  ]
-
   return (
-    <div className="flex flex-col gap-5 items-center justify-center w-full h-screen bg-zinc-300">
-      
-      {data.map((item, index) => (
-        <div key = {index} className='px-2 py-2 bg-white rounded-md w-100 h-fit'>
-        <h1 className='font-semibold px-2 py-2'>{item.name}</h1>
-        <p className='text-xs mt-1 ml-2'> {item.description}</p>
-        <button onClick={onhandler} className="bg-blue-500 text-xs text-white mt-1 px-2 py-2 rounded-md ml-2 hover:bg-blue-600">Download now</button>
-      </div>
-
-      ))}
-
-      
+    <div>
+     
     </div>
   )
 }
