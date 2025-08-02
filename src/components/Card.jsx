2a7bc70097d49bbdadf26548ea5b5dc38a5d190f
MrSanketPrajapatissp <125129649+mrsanketprajapati@users.noreply.github.com>
2025-08-02 14:45:31 +0530

    init

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,51 +1,31 @@
-import React from "react";
+import React from 'react'
 
 function Card() {
+
+  const onhandler = () => {
+    alert("Download started!");
+  }
+
   const data = [
-    {
-      name: "Amazon Basic",
-      description: " h-screen flex items-center justify-center",
-      url: "https://images.unsplash.com/photo-1633174524827-db00a6b7bc74?q=80&w=1196&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-      instock: true
-    },
-
-    {
-      name: "Daily Wear",
-      description: " h-screen flex items-center justify-center",
-      url: "https://images.unsplash.com/photo-1661733197946-30b64aab3ee7?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NHx8ZGFpbHklMjB3ZWFyfGVufDB8fDB8fHww",
-       instock: false
-    },
-
-    {
-      name: "Amazon Basic",
-      description: " h-screen flex items-center justify-center",
-      url: "https://images.unsplash.com/photo-1561154464-82e9adf32764?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Nnx8QXBwbGUlMjBkZXZpY2V8ZW58MHx8MHx8fDA%3D",
-      instock: true
-    },
-  ];
+    {name: "Mahi ve", description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit."},
+    {name: "O rabba ve", description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit."}
+  ]
 
   return (
-    <div className="w-full h-screen flex items-center justify-center gap-10 bg-zinc-200 ">
-      {data.map((value, index) => (
-        <div
-          key={index}
-          className="w-52  bg-zinc-100 rounded-md overflow-hidden"
-        >
-          <div className="w-full h-32 bg-zinc-300 ">
-            <img src={value.url} className="w-full h-full object-cover"></img>
-          </div>
-          <div className="w-full px-3 py-4">
-            <h2 className="font-semibold">{value.name}</h2>
-            <p className="text-xs mt-5">{value.description}</p>
-            <button className={`px-3 py-2 mt-3 ${value.instock ? 'bg-sky-600' : 'bg-red-500'} rounded text-xs text-zinc-100 cursor-pointer `}>
-            {value.instock ? "Instock": "Out of Stock"  }
-          </button>
-          </div>
-          
-        </div>
+    <div className="flex flex-col gap-5 items-center justify-center w-full h-screen bg-zinc-300">
+      
+      {data.map((item, index) => (
+        <div key = {index} className='px-2 py-2 bg-white rounded-md w-100 h-fit'>
+        <h1 className='font-semibold px-2 py-2'>{item.name}</h1>
+        <p className='text-xs mt-1 ml-2'> {item.description}</p>
+        <button onClick={onhandler} className="bg-blue-500 text-xs text-white mt-1 px-2 py-2 rounded-md ml-2 hover:bg-blue-600">Download now</button>
+      </div>
+
       ))}
+
+      
     </div>
-  );
+  )
 }
 
-export default Card;
+export default Card
